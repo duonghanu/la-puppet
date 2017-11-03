213de96ff8e679e1ce868fbf822e6c29118a3a78
root <root@duonghanu1.mylabserver.com>
2017-11-03 09:19:06 +0000

    add new file

--- manifests/init.pp
+++ manifests/init.pp
@@ -46,6 +46,10 @@ class ssh(
   String $package_name = $::ssh::params::package_name,
   String $service_name = $::ssh::params::service_name
 ) inherits ::ssh::params {
-  class {'::ssh::install': } -> 
-  class {'::ssh::service':}
+  class {'::ssh::install': } 
+  class {'::ssh::service':
+#    require => Class['::ssh::install']
+  }
+ 
+  Class['::ssh::install'] -> Class['::ssh::service']
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,37 @@
 class ssh::params {
-  case $facts['os']['family'] {
-    'Debian': {
-      $package_name = 'openssh-server'
-      $service_name = 'ssh' 
-    }
-    'RedHat': {
-      $package_name = 'openssh-server'
-      $service_name = 'sshd'
-     }
-    default: {
-      fail("${facts['operatingsystem']} is not supported")
-    }
-  }  
+
+#############################################################
+
+
+#############################################################
+
+#############################################################
+#  if $facts['os']['family'] == 'Debian' {
+#    $package_name = 'openssh-server'
+#    $service_name = 'ssh' 
+#  }
+#  elsif $facts['os']['family'] == 'Redhat' {
+#    $package_name = 'openssh-server'
+#    $service_name = 'sshd' 
+#  }
+#  else {
+#    fail("${facts['operatingsystem']} is not supported")
+#  }
+#############################################################
+
+#############################################################
+#  case $facts['os']['family'] {
+#    'Debian': {
+#      $package_name = 'openssh-server'
+#      $service_name = 'ssh' 
+#    }
+#    'RedHat': {
+#      $package_name = 'openssh-server'
+#      $service_name = 'sshd'
+#     }
+#    default: {
+#      fail("${facts['operatingsystem']} is not supported")
+#    }
+#  }  
+#############################################################
 }
